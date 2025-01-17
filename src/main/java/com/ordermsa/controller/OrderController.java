ecdfa1b24fdd4af30c77d8e3c8c7099b6624f842
Galmaeki <kjh09800@gmail.com>
2025-01-17 17:54:13 +0900

    Controller 변경 - 헬스체크 추가 - 누락된 어노테이션 추가 - 오타 수정

--- src/main/java/com/ordermsa/controller/OrderController.java
+++ src/main/java/com/ordermsa/controller/OrderController.java
@@ -6,22 +6,27 @@ import com.ordermsa.dto.OrderRequest;
 import com.ordermsa.dto.OrderResponse;
 import com.ordermsa.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RequestMapping("/order-service")
 @RequiredArgsConstructor
+@RestController
 public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/{userId]/orders")
+    @GetMapping("/healthcheck")
+    public String status() {
+        return "Working Now";
+    }
+
+    @PostMapping("/{userId}/orders")
     public ResponseEntity<OrderResponse> createOrder(@PathVariable String userId, @RequestBody OrderRequest orderRequest) {
         OrderDTO dto = new OrderDTO(orderRequest);
         dto.setUserId(userId);
